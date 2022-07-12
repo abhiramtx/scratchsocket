3daa952e3bff4cd68e4d609e0daa300aff71fcbb
Air_heads <106994329+abhiramtx@users.noreply.github.com>
2022-07-12 18:15:50 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="scratchsocket",
     url="none",
-    version="0.1.0",
+    version="0.3.0",
     author="Abhiram V",
     license="MIT",
     description="Establish cloud connections with scratch.mit.edu!",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     classifiers=[
